d3f515a88e58b3f78566d80f11a9bf9d7077560d
GodtierGD <par3ll3l@gmail.com>
2026-09-04 09:33:08 +0100

    test2

---
+++ JS/list.js
@@ -0,0 +1,36 @@
+const demonList = document.getElementById("demon-list");
+
+demons.forEach(demon => {
+    const card = document.createElement("div");
+
+    card.classList.add("demon-card");
+
+    card.innerHTML = `
+        <h2>${demon.position}. ${demon.name}</h2>
+        <p>Creator: ${demon.creator}</p>
+        <p>Difficulty: ${demon.difficulty}</p>
+    `;
+
+    demonList.appendChild(card);
+});
+
+const demons = [
+    {
+        name: "Bloodbath",
+        creator: "Riot",
+        difficulty: "Extreme Demon",
+        position: 1
+    },
+    {
+        name: "Slaughterhouse",
+        creator: "icedcave",
+        difficulty: "Extreme Demon",
+        position: 2
+    },
+    {
+        name: "Tidal Wave",
+        creator: "OniLink",
+        difficulty: "Extreme Demon",
+        position: 3
+    }
+];
