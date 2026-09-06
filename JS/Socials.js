7bf76dfac9e7ed9e13967d6c39e6ba0f93de5a2d
GodtierGD <par3ll3l@gmail.com>
2026-09-06 23:20:34 +0100

    up988

--- JS/Socials.js
+++ JS/Socials.js
@@ -17,10 +17,9 @@ const socialsData = [
     { label: "Main Channel",     url: "https://www.youtube.com/@GodtierGD",  icon: "Resources/Icons/icon-youtube.png" },
     { label: "Clips + Progress", url: "https://www.youtube.com/@Godgmd",  icon: "Resources/Icons/icon-youtube.png" },
     { label: "Aredl Account",    url: "https://aredl.net/profile/user/godtiergd",    icon: "Resources/Icons/icon-aredl.png" },
-    { label: "Aredl Account",    url: "https://aredl.net/profile/user/godtiergd",    icon: "Resources/Icons/icon-aredl.png" },
-    { label: "Aredl Account",    url: "https://aredl.net/profile/user/godtiergd",    icon: "Resources/Icons/icon-aredl.png" },
-    { label: "Aredl Account",    url: "https://aredl.net/profile/user/godtiergd",    icon: "Resources/Icons/icon-aredl.png" },
-    { label: "Aredl Account",    url: "https://aredl.net/profile/user/godtiergd",    icon: "Resources/Icons/icon-aredl.png" },
+    { label: "Aredl Clan",    url: "https://aredl.net/profile/clan/f362d293-cb04-40e9-b86d-5ef2001a596c",    icon: "Resources/Icons/icon-aredl.png" },
     { label: "Clan Discord",     url: "https://discord.gg/GyFYMrrRE5",   icon: "Resources/Icons/icon-discord.png" },
-    { label: "YT Discord",     url: "https://discord.gg/GyFYMrrRE5",   icon: "Resources/Icons/icon-discord.png" }
+    { label: "YT Discord",     url: "https://discord.gg/GyFYMrrRE5",   icon: "Resources/Icons/icon-discord.png" },
+    { label: "Github",     url: "https://github.com/GodtierGD",   icon: "Resources/Icons/icon-github.png" },
+    { label: "Twitter (dead)",     url: "https://x.com/GDgodtier/all",   icon: "Resources/Icons/icon-twitter.png" }
 ];
