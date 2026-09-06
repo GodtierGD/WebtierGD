0ecf26a4c595cddcf1cd064196eef33750862b0c
GodtierGD <par3ll3l@gmail.com>
2026-09-06 19:29:39 +0100

    up94325

--- JS/Changelog.js
+++ JS/Changelog.js
@@ -4,12 +4,12 @@
    Edit this file whenever you want to add a new update
    to the homepage changelog. Nothing else needs to
    change - Render.js reads this automatically.
-
+ 
    Add a new object to the TOP of the array for the
    newest date. If a date already has entries, just add
    another object to that date's "entries" array instead
    of creating a new date block.
-
+ 
    Example:
    {
        date: "7/9/2026",
@@ -19,25 +19,163 @@
        ]
    }
 ==================================================== */
-
+ 
 const changelogData = [
     {
-        date: "6/9/2026",
+        date: "22/8/2026",
         entries: [
-            { level: "Kowareta", info: "was placed at #1" }
+            { level: "Kowareta", info: "was placed at #1 (Above Citra)" }
         ]
     },
     {
-        date: "3/9/2026",
+        date: "15/8/2026",
         entries: [
-            { level: "Sonic Wave", info: "was lowered from #34 to #36 (Above level & below level2)" }
+            { level: "Sonic Wave", info: "was placed at #2 (Above Arctic Lights & Below Citra)" },
+            { level: "Wasureta", info: "was raised from #5 to #4 (Above Hyper Paracosm & Below Arctic Lights)" }
         ]
     },
     {
-        date: "2/9/2026",
+        date: "9/8/2026",
         entries: [
-            { level: "Citra", info: "was placed at #45" },
-            { level: "Nyutopia", info: "was placed at #1" }
+            { level: "To The Blue Moon", info: "was placed at #16 (Above Bloodbath & Below Sigma Interface)" }
+        ]
+    },
+    {
+        date: "7/8/2026",
+        entries: [
+            { level: "Eleos", info: "was placed at #10 (Above Blade Of Justice & Below Athanatos)" }
+        ]
+    },
+    {
+        date: "5/8/2026",
+        entries: [
+            { level: "BBF", info: "was placed at #23 (Above Volume & Below Final Epilogue)" }
+        ]
+    },
+    {
+        date: "4/8/2026",
+        entries: [
+            { level: "Volume", info: "was placed at #32 (Above The Flawless & Below Final Epilogue)" },
+            { level: "Bausha Vortex", info: "was placed at #6 (Above Quantum Processing & Below Catalyze)" }
+        ]
+    },
+    {
+        date: "2/8/2026",
+        entries: [
+            { level: "BBF", info: "was placed at #23 (Above Crowd Control & Below Down Bass)" }
+        ]
+    },
+    {
+        date: "19/7/2026",
+        entries: [
+            { level: "Catalyze", info: "was placed at #5 (Above Quantum Processing & Below Wasureta)" }
+        ]
+    },
+    {
+        date: "9/7/2026",
+        entries: [
+            { level: "Cat Planet", info: "was placed at #30 (Above Proteus & Below The Flawless)" }
+        ]
+    },
+    {
+        date: "8/7/2026",
+        entries: [
+            { level: "Aftermath", info: "was placed at #19 (Above NecropoliX & Below Robi)" },
+            { level: "Quantum Processing", info: "was placed at #5 (Above WcropoliX & Below Wasureta)" }
+        ]
+    },
+    {
+        date: "4/7/2026",
+        entries: [
+            { level: "Red World Rebirth", info: "was placed at #30 (Above Raindance & Below Breakout)" }
+        ]
+    },
+    {
+        date: "3/7/2026",
+        entries: [
+            { level: "Blade Of Justice", info: "was placed at #7 (Above Sakupen Hell & Below Athanatos)" }
+        ]
+    },
+    {
+        date: "26/6/2026",
+        entries: [
+            { level: "Final Epilogue", info: "was placed at #25 (Above The Flawless & Below Brutal)" }
+        ]
+    },
+    {
+        date: "25/6/2026",
+        entries: [
+            { level: "WcropoliX", info: "was placed at #5 (Above Athanatos & Below Wasureta)" }
+        ]
+    },
+    {
+        date: "23/6/2026",
+        entries: [
+            { level: "NecropoliX", info: "was placed at #16 (Above Down Bass & Below Robi)" }
+        ]
+    },
+    {
+        date: "21/6/2026",
+        entries: [
+            { level: "Brutal", info: "was placed at #22 (Above The Flawless & Below Azurite)" },
+            { level: "Breakout", info: "was placed at #25 (Above Raindance & Below Proteus)" }
+        ]
+    },
+    {
+        date: "19/6/2026",
+        entries: [
+            { level: "Uprise", info: "was placed at #18 (Above Forbidden Isle & Below Crowd Control)" }
+        ]
+    },
+    {
+        date: "18/6/2026",
+        entries: [
+            { level: "Acu", info: "was placed at #27 (Above Sweater Weather & Below Artificial Dream)" },
+            { level: "Proteus", info: "was placed at #23 (Above Raindance & Below The Flawless)" }
+        ]
+    },
+    {
+        date: "16/6/2026",
+        entries: [
+            { level: "Down Bass", info: "was placed at #16 (Above Crowd Control & Below Robi)" },
+            { level: "Artificial Dream", info: "was placed at #24 (Above Sweater Weather & Below Cataclysm)" }
+        ]
+    },
+    {
+        date: "15/6/2026",
+        entries: [
+            { level: "Robi", info: "was placed at #15 (Above Crowd Control & Below ACFL)" }
+        ]
+    },
+    {
+        date: "11/6/2026",
+        entries: [
+            { level: "Arctic Lights", info: "was placed at #2 (Above Hyper Paracosm & below Citra)" }
+        ]
+    },
+    {
+        date: "9/6/2026",
+        entries: [
+            { level: "Citra", info: "was placed at #1 (Above Hyper Paracosm)" }
+        ]
+    },
+    {
+        date: "21/5/2026",
+        entries: [
+            { level: "Illusion Circles III", info: "was placed at #5 (Above Illusion Circles & below Sakupen Hell)" }
+        ]
+    },
+    {
+        date: "14/5/2026",
+        entries: [
+            { level: "Illusion Circles", info: "was raised to #5 (Above Sigma Interface & below Sakupen Hell)" }
+        ]
+    },
+    {
+        date: "13/5/2026",
+        entries: [
+            { level: "List Created.", info: "Top 3: Hyper Paracosm, Wasureta, Athanatos (x2)" }
         ]
     }
 ];
+ 
