e226d8c41498b4a8517500861085bc3246ff157c
GodtierGD <par3ll3l@gmail.com>
2026-09-08 11:00:28 +0100

    up543543

--- JS/Levels/citra.js
+++ JS/Levels/citra.js
@@ -8,7 +8,7 @@ export const citra = {
 
     thumbnail: "./Resources/TB/citra.png",
     tags: [
-        "./Resources/Tags/tagHARDEST2.png"
+        "./Resources/Tags/tagHARDEST3.png"
     ],
 
     link: "https://www.youtube.com/watch?v=tme52R04Hj0",
--- JS/Levels/kowareta.js
+++ JS/Levels/kowareta.js
@@ -8,7 +8,7 @@ export const kowareta = {
  
     thumbnail: "./Resources/TB/kowareta.png",
     tags: [
-        "./Resources/Tags/tagHARDEST2.png"
+        "./Resources/Tags/tagHARDEST3.png"
     ],
  
     link: "https://www.youtube.com/watch?v=1CSLD4SHJP0",
