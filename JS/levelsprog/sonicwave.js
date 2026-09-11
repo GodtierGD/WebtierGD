b581f61a43df8d26485f99a6e117aef0e08faeea
GodtierGD <par3ll3l@gmail.com>
2026-09-11 11:57:20 +0100

    up67576

---
+++ JS/levelsprog/sonicwave.js
@@ -0,0 +1,10 @@
+export const sonicwave = {
+    name: "Sonic Wave",
+    gradient: { start: "#c98a52", end: "#f2c39a" },
+    thumbnail: "./Resources/TB/sonicwave.png",
+    stats: {
+        attempts: "28.9k",
+        percent: 34,
+        line2: "50-100, 34-56"
+    }
+};
