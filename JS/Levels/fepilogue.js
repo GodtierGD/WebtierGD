76701201205aed6db9faa3d967054a92d8b0ae36
GodtierGD <par3ll3l@gmail.com>
2026-09-11 09:24:58 +0100

    up54654

--- JS/Levels/fepilogue.js
+++ JS/Levels/fepilogue.js
@@ -8,7 +8,7 @@ export const fepilogue = {
  
     thumbnail: "./Resources/TB/replacement.png",
  
-    link: "https://www.youtube.com/watch?v=sjodjBwBKrw",
+    link: "https://www.youtube.com/watch?v=VuAWt1mDRac&t=2s",
     rating: "1/10",
  
     stats: {
