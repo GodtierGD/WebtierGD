9f22cd3805a3ef29fc8845474ef389dc89123441
GodtierGD <par3ll3l@gmail.com>
2026-09-14 21:29:20 +0100

    up54353

---
+++ JS/levelsprog/hs.js
@@ -0,0 +1,15 @@
+export const hs = {
+    name: "Heavenly Sakupen",
+    
+    gradient: 
+    { start: "#6d6d6d", 
+        end: "#d3d3d3" },
+
+    thumbnail: "./Resources/TB/no thumbnail.png",
+
+    stats: {
+        attempts: "~1.5k",
+        percent: 27,
+        line2: "33-52, 56-100 | Would place #1"
+    }
+};
--- JS/prog.js
+++ JS/prog.js
@@ -14,6 +14,7 @@
 const progOrder = [
     "pagoda",
     "whirlfate",
+    "hs",
     "cog",
     "wow",
     "silcata",
