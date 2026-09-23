032f36e865b9652b50cb30950682d16238c2dab4
GodtierGD <par3ll3l@gmail.com>
2026-09-23 09:27:11 +0100

    up63463

---
+++ JS/levelsprog/dubkore.js
@@ -0,0 +1,15 @@
+export const dubkore = {
+    name: "Dubkore X",
+
+    gradient: 
+    { start: "#6d6d6d", 
+        end: "#d3d3d3" },
+
+    thumbnail: "./Resources/TB/no thumbnail.png",
+
+    stats: {
+        attempts: "~860",
+        percent: 0,
+        line2: "54-100 | Would place #11"
+    }
+};
--- JS/levelsprog/spacial.js
+++ JS/levelsprog/spacial.js
@@ -5,7 +5,7 @@ export const spacial = {
     { start: "#6d6d6d", 
         end: "#d3d3d3" },
 
-    thumbnail: "./Resources/TB/placeholder.png",
+    thumbnail: "./Resources/TB/no thumbnail.png",
 
     stats: {
         attempts: "~3k",
